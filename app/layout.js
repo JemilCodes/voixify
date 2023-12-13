f28a08c84cec0051f0ffb198800b79d19e787a20
jemilu ishaq <jemilCodes>
2023-12-13 19:57:46 +0100

    deploy

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className="center">{children}</body>
     </html>
   );
